c5894c428b1dd077fc311b02c49a437238eb1866
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2022-04-05 03:33:45 +0500

    Second Commit

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 function changeParagraph(){
     document.getElementById("demo").innerHTML= "My name is Moin";
+    document.getElementById("image1").src = "images/Second Certificate.png";
 }
+// index of that tag / class name is necessary (not for all)
 function changeHeaderOne(){
-    document.getElementsByTagName("h1")[0].innerHTML = "Hello Mates"; // index of that tag is necessary
+    document.getElementsByTagName("h1")[0].innerHTML = "Hello Mates";
 }
+function changeParaColor() {
+    document.getElementsByClassName("para-color")[0].style.color = "blue";
+}
+function changeParaColorQuery() {
+    document.querySelectorAll("p.query")[0].style.color = "green";
+}
+const x = document.forms["frm1"];
+let text = "";
+for (let i = 0; i < x.length ;i++) {
+  text += x.elements[i].value + "<br>";
+}
+document.getElementById("check").innerHTML = text;
