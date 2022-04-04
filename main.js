29496858736c0ff794e435813644761c1333aadd
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2022-04-04 19:26:09 +0500

    First Commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function changeParagraph(){
+    document.getElementById("demo").innerHTML= "My name is Moin";
+}
+function changeHeaderOne(){
+    document.getElementsByTagName("h1")[0].innerHTML = "Hello Mates"; // index of that tag is necessary
+}
